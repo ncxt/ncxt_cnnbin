aaa2b5c64cca42c9d3831c6b80a112fcdf49987f
Axel Ekman <axel.ekman@iki.fi>
2020-12-14 22:05:58 -0800

    hyper tuning

--- cnnbin/cnnfilter.py
+++ cnnbin/cnnfilter.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from scipy.signal.windows import hann
-from tqdm import tqdm
+from tqdm import tnrange
+from tqdm.auto import tqdm
 
 from .dataset import N2NMultiPatches, N2NPatches
 from .patches import combine, split
@@ -305,7 +306,9 @@ class CNNbin:
         self.res_psnr = []
 
         self.model.train()
-        pbar = progress_bar(num_epochs, desc="PSNR ({:.3}/{:.3})".format(0.0, 0.0))
+        pbar = tnrange(
+            num_epochs, desc="PSNR ({:.3}/{:.3})".format(0.0, 0.0), leave=False
+        )
 
         for _ in pbar:
             self.epoch += 1
@@ -385,7 +388,7 @@ class CNNbin:
         else:
             patches_filt = np.zeros((patches.shape[0], *bin_block_size))
 
-        for index in rangebar(len(patches)):
+        for index in tnrange(len(patches), leave=False):
             patches_filt[index] = self.filter_patch(patches[index])
 
         return combine(patches_filt, bin_shape, sampling=sampling, windowfunc=hann)
